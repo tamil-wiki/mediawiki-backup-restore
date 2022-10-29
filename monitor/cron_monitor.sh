89ff2156d7eddf73c1988711ef7f7887228850f6
vijayabharathib <yajiv.vijay@gmail.com>
2022-10-30 01:43:54 +0530

    A working cron for monitoring

--- monitor/cron_monitor.sh
+++ monitor/cron_monitor.sh
@@ -1,8 +1,57 @@
 #! /usr/bin/env bash
 # set -x
 # set -e
-post_test_notification(){
-    curl -X POST https://discord.com/api/webhooks/970347000719609866/To3ULCRUJ9SCusjVUvyLCGrifSCAvH6VTowKJlUOP6rUDQeUxgLmL3XWMY_CRcYYWHRe \
-        -H 'Content-Type: application/json' \
-        -d '{"embeds":[{"color":"14365807","title":"cron","type":"rich","description":"Cron executed","fields":[{"name":"source","value":"db"},{"name":"time","value":"2022-10-06 12:00:00"},{"name":"status","value":"failed"},{"name":"size","value":"450MB"},{"name":"frequency","value":"hourly"}]}]}'
+
+export AWS_ACCESS_KEY_ID=$S3_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$S3_SECRET_ACCESS_KEY
+
+if [[ -z "$S3_ENDPOINT" ]]; then
+  AWS_ARGS=""
+else
+  AWS_ARGS="--endpoint-url $S3_ENDPOINT"
+fi
+
+list_s3() {
+  if [[ -z "$S3_PREFIX" ]]; then
+    S3_PATH= "s3://$S3_BUCKET"
+  else
+    S3_PATH="s3://$S3_BUCKET/$S3_PREFIX"
+  fi
+
+  if [[ ! -z "$1" ]]; then
+    S3_PATH="$S3_PATH/$1"
+  fi
+
+  echo "Listing all files from $S3_PATH"
+  aws $AWS_ARGS s3 ls $S3_PATH/ --human-readable
 }
+s3_latest_mediawiki_dump_backup_record=$(list_s3 hourly | tail -1) 
+echo "Latest mediawiki dump record: $s3_latest_mediawiki_dump_backup_record"
+s3_latest_sql_dump_backup_record=$(list_s3 hourly | tail -2 | head -1) 
+echo "Latest sql dump record: $s3_latest_sql_dump_backup_record"
+
+t1=$(echo $s3_latest_mediawiki_dump_backup_record | awk '{print substr($0,1,19)}')
+mediawiki_dump_time=$(date -d "$t1" +%s)
+t2=$(echo $s3_latest_sql_dump_backup_record | awk '{print substr($0,1,19)}')
+sql_dump_time=$(date -d "$t2" +%s)
+current_date_time=$(date +%s)
+seconds_since_mediawiki_dump=$((current_date_time-mediawiki_dump_time))
+seconds_since_sql_dump=$((current_date_time-sql_dump_time))
+
+# basic calculator bc is needed to retain floating point values in hours 
+hours_since_mediawiki_dump=$(bc <<< "scale=2; $seconds_since_mediawiki_dump/60/60")
+hours_since_sql_dump=$(bc <<< "scale=2; $seconds_since_sql_dump/60/60")
+echo "Hours since mediawiki Dump: $hours_since_mediawiki_dump"
+echo "Hours since sql Dump: $hours_since_sql_dump"
+
+# check if the backup is within the required timeframe 
+minimum_backup_time_requirement=1.5 
+mediawiki_backup_delayed=$(echo "$hours_since_mediawiki_dump > $minimum_backup_time_requirement" |bc -l)
+sql_backup_delayed=$(echo "$hours_since_sql_dump > $minimum_backup_time_requirement" |bc -l)
+
+# if not, notify 
+if [[ $((mediawiki_backup_delayed + sql_backup_delayed)) > 0 ]] ; then 
+    curl -X POST $DISCORD_NOTIFICATION_URL -H 'Content-Type: application/json' -d '{"embeds":[{"color":"14365807","title":"MediaWiki Backup","type":"rich","description":"Backup Cron Testing","fields":[{"name":"Mediawiki Backup Time","value":"'"$hours_since_mediawiki_dump hours ago"'"},{"name":"DB Bakup Time","value":"'"$hours_since_sql_dump hours ago"'"},{"name":"frequency","value":"hourly"},{"name":"S3 Bucket","value":"'"$S3_PREFIX"'"}]}]}'
+fi
+
+# if there is a backup , no notification is sent. 
--- monitor/entrypoint.sh
+++ monitor/entrypoint.sh
@@ -21,12 +21,18 @@ fi
 # env files needed for aws cli
 export AWS_ACCESS_KEY_ID=$S3_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$S3_SECRET_ACCESS_KEY
+
 if [[ ! -z "$S3_REGION" ]]; then
   export AWS_DEFAULT_REGION=$S3_REGION
 fi
+
 source /cron_monitor.sh
 echo "#! /usr/bin/env bash" > /root/.bashrc
 echo "source /cron_monitor.sh" >> /root/.bashrc
-exec bash
+echo "${CRON_MONITOR_TIME} bash /cron_monitor.sh 2>&1 >> /dev/stdout" >> /tmp/crontab.conf
+
+crontab /tmp/crontab.conf
+echo "Running cron task manager in foreground"
+exec crond -f -L /dev/stdout
 # assume user want to run his own process, for example a `bash` shell to explore this image
 exec "$@"
