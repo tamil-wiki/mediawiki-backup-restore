16633db0cc104399370b3e6a3ac6b7bea61ddd53
vijayabharathib <yajiv.vijay@gmail.com>
2024-05-05 13:46:17 +0530

    add alert when backup recovers

--- monitor/cron_monitor.sh
+++ monitor/cron_monitor.sh
@@ -2,6 +2,8 @@
 # set -x
 # set -e
 
+# File path to store the variable value
+
 export AWS_ACCESS_KEY_ID=$S3_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$S3_SECRET_ACCESS_KEY
 
@@ -39,8 +41,8 @@ seconds_since_mediawiki_dump=$((current_date_time-mediawiki_dump_time))
 seconds_since_sql_dump=$((current_date_time-sql_dump_time))
 
 # basic calculator bc is needed to retain floating point values in hours 
-hours_since_mediawiki_dump=$(bc <<< "scale=2; $seconds_since_mediawiki_dump/60/60")
-hours_since_sql_dump=$(bc <<< "scale=2; $seconds_since_sql_dump/60/60")
+hours_since_mediawiki_dump=$(echo "scale=2; $seconds_since_mediawiki_dump/60/60" | bc)
+hours_since_sql_dump=$(echo "scale=2; $seconds_since_sql_dump/60/60" | bc)
 echo "Hours since mediawiki Dump: $hours_since_mediawiki_dump"
 echo "Hours since sql Dump: $hours_since_sql_dump"
 
@@ -49,10 +51,71 @@ minimum_backup_time_requirement=1.5
 mediawiki_backup_delayed=$(echo "$hours_since_mediawiki_dump > $minimum_backup_time_requirement" |bc -l)
 sql_backup_delayed=$(echo "$hours_since_sql_dump > $minimum_backup_time_requirement" |bc -l)
 
-# if not, notify 
+hours_since_mediawiki_dump=$(( seconds_since_mediawiki_dump / 3600 ))
+remaining_seconds=$(( seconds_since_mediawiki_dump % 3600 ))
+minutes_since_mediawiki_dump=$(( remaining_seconds / 60 ))
+
+hours_since_sql_dump=$(( seconds_since_sql_dump / 3600 ))
+remaining_seconds=$(( seconds_since_sql_dump % 3600 ))
+minutes_since_sql_dump=$(( remaining_seconds / 60 ))
+
+ALERT_DIR="/alert"
+state_of_alert="$ALERT_DIR/state_of_alert.env"
+
+# Read the value of the variable from the file
+if [ -f "$state_of_alert" ]; then
+    ONGOING_BACKUP_DELAY_ALERT_NOTIFIED=$(<"$state_of_alert")
+else
+    ONGOING_BACKUP_DELAY_ALERT_NOTIFIED="false"
+fi
+
 if [[ $((mediawiki_backup_delayed + sql_backup_delayed)) > 0 ]] ; then 
-    curl -X POST $DISCORD_NOTIFICATION_URL -H 'Content-Type: application/json' -d '{"embeds":[{"color":"14365807","title":"MediaWiki Backup","type":"rich","description":"Hourly Backup Failed","fields":[{"name":"Mediawiki Backup Time","value":"'"$hours_since_mediawiki_dump hours ago"'"},{"name":"DB Bakup Time","value":"'"$hours_since_sql_dump hours ago"'"},{"name":"frequency","value":"hourly"},{"name":"S3 Bucket","value":"'"$S3_PREFIX"'"}]}]}'
-    echo "Notified the delay in backup cron"
+  result="Hourly Backup Failed"
+  notification_color="14365807" # red 
+  notify="true"
+  ONGOING_BACKUP_DELAY_ALERT_NOTIFIED="true"
+elif [ "$ONGOING_BACKUP_DELAY_ALERT_NOTIFIED" = "true" ]; then
+  result="Hourly Backup Recovered"
+  notification_color="5763719" # green
+  notify="true"
+  ONGOING_BACKUP_DELAY_ALERT_NOTIFIED="false"
+else 
+  ONGOING_BACKUP_DELAY_ALERT_NOTIFIED="false"
+  notify="false"
+fi 
+
+# Write the updated value back to the file
+echo "$ONGOING_BACKUP_DELAY_ALERT_NOTIFIED" > "$state_of_alert"
+
+# if not, notify 
+if [ "$notify" = "true" ] ; then 
+    curl -X POST $DISCORD_NOTIFICATION_URL -H 'Content-Type: application/json' \
+      -d '{"embeds":[{ 
+      "color":"'"$notification_color"'",
+      "title":"MediaWiki Backup",
+      "type":"rich",
+      "description":"'"$result"'",
+      "fields":[
+        {
+          "name":"Time Since Last Mediawiki Image Backup",
+          "value":"'"$hours_since_mediawiki_dump hours $minutes_since_mediawiki_dump minutes ago"'"
+          },{
+          "name":"Latest Mediawiki Image Backup Timestamp",
+          "value":"'"$t1"'"
+          },{
+          "name":"Time Since Last DB SQL Bakup",
+          "value":"'"$hours_since_sql_dump hours $minutes_since_sql_dump minutes ago"'"
+          },{
+          "name":"Latest DB SQL Backup Timestamp",
+          "value":"'"$t2"'"
+          },{
+          "name":"frequency",
+          "value":"hourly"
+          },{
+          "name":"S3 Bucket",
+          "value":"'"$S3_PREFIX"'"
+        }]}]}'
+    echo "Notification sent: $result"
 else 
     echo "No delay in hourly backup. Hence, no notification sent."
 fi
