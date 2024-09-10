33fd094d70c85b0113d7e97028c2aaead6e91d61
vijayabharathib <yajiv.vijay@gmail.com>
2024-09-10 21:12:06 +0530

    parameterise db image backups to run separately

--- backup.sh
+++ backup.sh
@@ -36,41 +36,54 @@ copy_s3 () {
   fi
   rm -f $SRC_FILE
 }
+
+db_backup () {
+  logger "Creating dump for ${MYSQLDUMP_DATABASE} from ${MYSQL_HOST}..."
+  DUMP_SQL_FILE="$BACKUP_DIR/$DUMP_START_TIME.dump.sql"
+  DUMP_FILE="$DUMP_SQL_FILE.gz"
+  mysqldump --single-transaction $MYSQLDUMP_OPTIONS $MYSQL_HOST_OPTS $MYSQL_SSL_OPTS $MYSQLDUMP_DATABASE > $DUMP_SQL_FILE
+  logger "Compressing $DUMP_SQL_FILE"
+  gzip -f "$DUMP_SQL_FILE"
+
+  if [ "$?" == "0" ]; then
+    S3_FILE="$DUMP_START_TIME.$FREQUENCY.dump.sql.gz"
+    copy_s3 $DUMP_FILE $S3_FILE $FREQUENCY
+  else
+    logger "Error creating mysqldump"
+  fi
+}
+
+image_backup () {
+  MEDIAWIKI_DIR="/mediawiki"
+  DUMP_FILE="$BACKUP_DIR/$DUMP_START_TIME.mediawiki.tar.gz"
+  # backup mediawiki folder
+  if [ -d $MEDIAWIKI_DIR ]; then
+    logger "Creating $DUMP_FILE from $MEDIAWIKI_DIR"
+    # Gzip mediawiki folder
+    tar -czf $DUMP_FILE -C $(dirname $MEDIAWIKI_DIR) $(basename $MEDIAWIKI_DIR)
+    if [ "$?" == "0" ]; then
+      S3_FILE="$DUMP_START_TIME.$FREQUENCY.mediawiki.tar.gz"
+      copy_s3 $DUMP_FILE $S3_FILE $FREQUENCY
+    else
+      logger "Error creating mediawiki"
+    fi
+  fi
+}
+
 FREQUENCY=${1:-hourly}
 MYSQL_HOST_OPTS="-h $MYSQL_HOST -P $MYSQL_PORT -u$MYSQL_USER -p$MYSQL_PASSWORD"
 DUMP_START_TIME=$(date +"%Y-%m-%dT%H%M%SZ")
 BACKUP_DIR="/backup"
-
 logger "Backup frequency ${FREQUENCY}"
 logger "Backup is started at ${DUMP_START_TIME}"
-logger "Creating dump for ${MYSQLDUMP_DATABASE} from ${MYSQL_HOST}..."
-DUMP_SQL_FILE="$BACKUP_DIR/$DUMP_START_TIME.dump.sql"
-DUMP_FILE="$DUMP_SQL_FILE.gz"
-mysqldump --single-transaction $MYSQLDUMP_OPTIONS $MYSQL_HOST_OPTS $MYSQL_SSL_OPTS $MYSQLDUMP_DATABASE > $DUMP_SQL_FILE
-logger "Compressing $DUMP_SQL_FILE"
-gzip -f "$DUMP_SQL_FILE"
 
-if [ "$?" == "0" ]; then
-  S3_FILE="$DUMP_START_TIME.$FREQUENCY.dump.sql.gz"
-  copy_s3 $DUMP_FILE $S3_FILE $FREQUENCY
-else
-  logger "Error creating mysqldump"
-fi
+if [[ "$DB_BACKUP_REQUIRED" == "true"  ]]; then
+  db_backup
+fi 
 
-MEDIAWIKI_DIR="/mediawiki"
-DUMP_FILE="$BACKUP_DIR/$DUMP_START_TIME.mediawiki.tar.gz"
-# backup mediawiki folder
-if [ -d $MEDIAWIKI_DIR ]; then
-  logger "Creating $DUMP_FILE from $MEDIAWIKI_DIR"
-  # Gzip mediawiki folder
-  tar -czf $DUMP_FILE -C $(dirname $MEDIAWIKI_DIR) $(basename $MEDIAWIKI_DIR)
-  if [ "$?" == "0" ]; then
-    S3_FILE="$DUMP_START_TIME.$FREQUENCY.mediawiki.tar.gz"
-    copy_s3 $DUMP_FILE $S3_FILE $FREQUENCY
-  else
-    logger "Error creating mediawiki"
-  fi
-fi
+if [[ "$IMAGE_BACKUP_REQUIRED" == "true"  ]]; then
+  image_backup
+fi 
 
 DUMP_END_TIME=$(date +"%Y-%m-%dT%H%M%SZ")
 logger "Backup is ends at ${DUMP_END_TIME}"
